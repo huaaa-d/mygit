2249375e23c43606a4f1eb081919184cd4f7b2aa
huaaa-d <2316502955@qq.com>
2022-10-09 21:51:22 +0800

    修改02

--- src/test/java/test01.java
+++ src/test/java/test01.java
@@ -3,5 +3,8 @@ public class test01 {
         System.out.println("test01...");
         System.out.println("test02...");
         System.out.println("test03...");
+        System.out.println("test04...");
+        System.out.println("test05...");
+
     }
 }
